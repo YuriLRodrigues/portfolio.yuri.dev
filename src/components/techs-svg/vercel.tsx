6516d3d73cc20da0e771318e7e88ee008f2d7848
YuriLRodrigues <rodriguesyuri769@gmail.com>
2025-03-05 21:21:28 -0300

    feat: :sparkles: components of svg icons

---
+++ src/components/techs-svg/vercel.tsx
@@ -0,0 +1,16 @@
+export const VercelSvg = () => {
+  return (
+    <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        {" "}
+        <path d="M12 1L24 22H0L12 1Z" fill="#383838"></path>{" "}
+      </g>
+    </svg>
+  );
+};
