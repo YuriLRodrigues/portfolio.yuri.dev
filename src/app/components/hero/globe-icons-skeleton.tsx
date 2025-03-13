cf2e3e571633c990251e0be584bd5d35adaf6905
YuriLRodrigues <rodriguesyuri769@gmail.com>
2025-03-13 15:21:00 -0300

    style: :lipstick: remove color of bg

--- src/app/components/hero/globe-icons-skeleton.tsx
+++ src/app/components/hero/globe-icons-skeleton.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from '@/components/ui/skeleton'
 
 export const GlobeIconsSkeleton = () => {
   return (
-    <div className="relative ml-auto flex size-full w-full max-w-xl items-center justify-center overflow-hidden bg-green-500 sm:px-20 xl:mt-0 xl:justify-end">
+    <div className="relative mx-auto flex size-full w-full max-w-xl items-center justify-center overflow-hidden pb-20 sm:ml-auto sm:px-20 sm:pb-0 xl:mt-0 xl:justify-end">
       <Skeleton className="mx-auto h-64 w-64 rounded-full sm:h-96 sm:w-96" />
     </div>
   )
